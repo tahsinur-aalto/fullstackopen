0973f19f10109826fe61c1d9758d211287201a0f
tahsinur-aalto <tahsinur.rahman@aalto.fi>
2020-11-14 18:14:43 +0600

    Part1 modified

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+
+  return (
+      <>
+        <h1>{props.course}</h1>
+      </>
+  )
+}
+
+const Part = (props) => {
+  
+  return (
+      <div>
+          <p>
+              {props.part.name} {props.part.exercises}
+          </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+
+  let totalSum = 0;
+
+  props.parts.forEach(part => {
+      totalSum += part.exercises
+  })
+
+  return (
+      <>
+        <p>Number of exercises {totalSum}</p>
+      </>
+          
+  )
+}
+
+const Content = (props) => {
+  return (
+      <div>
+         <Part part={props.parts[0]}/>
+         <Part part={props.parts[1]}/>
+         <Part part={props.parts[2]}/>
+      </div>
+  )
+
+}
 
 
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
+  
+    return (
+      <div>
+        <Header course={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts} />
+      </div>
+    )
+  }
 
-
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'))
