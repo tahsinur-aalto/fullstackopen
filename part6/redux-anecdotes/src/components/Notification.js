7b85a80043510c2d58bc3184264ed8b2ba6743fa
tahsinur-aalto <tahsinur.rahman@aalto.fi>
2021-01-09 15:05:11 +0600

    Fixed bugs in part6

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -8,6 +8,7 @@ const Notification = () => {
     padding: 10,
     borderWidth: 1
   }
+  
   return (
     <div style={style}>
       {notification}
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,25 +1,38 @@
-const notifReducer = (state = 'Test Notification', action) => { 
-    switch (action.type) {
-        case 'CREAT_NOTIF':
-          return action.data
-        default:
-          return state
-      }
-    }
+const initialState = 'Test Notification'
+
+const asObject = (notif) => {
+  return {
+    content: notif,
+    id: (100000 * Math.random()).toFixed(0)
+  }
+}
+
+
+const notifReducer = (state = initialState, action) => { 
+  switch (action.type) {
+    case 'CREATE_NOTIF':
+      // return state.concat(asObject(action.data))
+      return state
+    case 'REMOVE_NOTIF':
+      return state.filter((notif) => notif.id !== action.id)
+    default:
+      return state
+  }
+}
     
-    export const setNotification = (content) => {
-      return dispatch=>{
-        dispatch({
-          type: 'SET_NOTIFICATION',
-          data: content
-        })
-          setTimeout(() => {
-            dispatch({
-              type: 'SET_NOTIFICATION',
-              data: null
-            })
-          }, 5000)
-     }
-    }
-    
-    export default notifReducer
+export const setNotification = (content) => {
+  return {
+    type: 'CREATE_NOTIF',
+    data: content
+  }
+}
+
+export const removeNotification = (id) => {
+  return {
+    type: 'CREATE_NOTIF',
+    id: id
+  }     
+}
+
+
+export default notifReducer
